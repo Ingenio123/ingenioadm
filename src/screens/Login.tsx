6fc15d86cea66279e34f4863dfec77e3f7b6c003
Ingenio123 <83844889+Ingenio123@users.noreply.github.com>
2022-01-19 10:36:58 -0500

    commit

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,7 +1,11 @@
 import LoginArea from "../components/Login/LoginArea";
+import { Link } from "react-router-dom";
 function Login() {
   return (
     <div>
+      <Link to="/admin">Admin</Link>
+      <br />
+      <Link to="/admin/teacher">Admin Teacher</Link>
       <LoginArea />
     </div>
   );
