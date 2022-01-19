5bb488bef284dd198ddb898f9e60fef3cd7a789a
Ingenio123 <83844889+Ingenio123@users.noreply.github.com>
2022-01-19 11:20:47 -0500

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import theme from "./theme";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { RecoilRoot } from "recoil";
+import Login from "./screens/Login";
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
       <BrowserRouter>
         <ChakraProvider theme={theme}>
           <App />
+          <Switch>
+            <Route exact path="/login" component={Login}></Route>
+          </Switch>
         </ChakraProvider>
       </BrowserRouter>
     </RecoilRoot>
